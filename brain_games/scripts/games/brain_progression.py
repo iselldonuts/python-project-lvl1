b7727707254a74c5df47d39417f37e57a1835598
Valentin <mananathen@gmail.com>
2020-08-11 09:15:32 +0300

    new game brain-progression

---
+++ brain_games/scripts/games/brain_progression.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import random
+from ..game import start
+
+LOWER_BOUND = 0
+UPPER_BOUND = 100
+LOWER_BOUND_STEP = 1
+UPPER_BOUND_STEP = 10
+LENGTH = 10
+
+
+def get_question_data():
+    start = random.randint(LOWER_BOUND, UPPER_BOUND)
+    step = random.randint(LOWER_BOUND_STEP, UPPER_BOUND_STEP)
+    progression = list(range(start, start + step * LENGTH, step))
+    position = random.randint(0, LENGTH - 1)
+
+    def remove_target_element(iv):
+        i, v = iv
+        return '..' if i == position else str(v)
+
+    title = ' '.join(map(remove_target_element, enumerate(progression)))
+    answer = str(progression[position])
+    return title, answer
+
+
+def main():
+    description = 'What number is missing in the progression?'
+    get_question_data()
+    start(description, get_question_data)
+
+
+if __name__ == '__main__':
+    main()
